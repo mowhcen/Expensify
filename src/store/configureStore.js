b21a78e68d8c72c80bd2628d571b9ae950008820
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-17 12:06:16 +0330

    feat: create redux store for the app

---
+++ src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { combineReducers, createStore } from "redux";
+
+import expensesReducer from "../reducers/expenses";
+import filterReducer from "../reducers/filters";
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filterReducer,
+        }),
+    );
+
+    return store;
+};
