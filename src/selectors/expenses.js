08700c088f226c39f8546065cae899f7d6235a25
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-17 12:09:02 +0330

    feat: filter visible  expense for user

---
+++ src/selectors/expenses.js
@@ -0,0 +1,30 @@
+import moment from "moment";
+
+// get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses
+        .filter((expense) => {
+            const createdAtMoment = moment(expense.createAt);
+            const startDateMatch = startDate
+                ? startDate.isSameOrBefore(createdAtMoment, "day")
+                : true;
+
+            const endDateMatch = endDate
+                ? endDate.isSameOrAfter(createdAtMoment, "day")
+                : true;
+            const textMatch = expense.description
+                .toLowerCase()
+                .includes(text.toLowerCase());
+
+            return startDateMatch && endDateMatch && textMatch;
+        })
+        .sort((a, b) => {
+            if (sortBy === "date") {
+                return a.createAt < b.createAt ? 1 : -1;
+            } else if (sortBy === "amount") {
+                return a.amount < b.amount ? 1 : -1;
+            }
+        });
+};
+
+export default getVisibleExpenses;
