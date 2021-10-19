32110e361dfdbaeda97f0f32c1e08bc8f2239d5b
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-19 13:07:21 +0330

    reafactor: delete dumy data

--- src/main.js
+++ src/main.js
@@ -12,34 +12,6 @@ import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
-store.dispatch(
-    addExpense({
-        description: "Water bill",
-        note: "payment for water bill",
-        amount: 4500,
-    })
-);
-
-store.dispatch(
-    addExpense({
-        description: "Gas bill",
-        note: "payment for gas bill",
-        createAt: 1000,
-    })
-);
-
-store.dispatch(
-    addExpense({
-        description: "Rent",
-        note: "payment for gas bill",
-        amount: 109500,
-    })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
