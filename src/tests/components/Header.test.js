6c7a68a94a80f00c324690040352143b4817a6f8
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:27:07 +0330

    test: should call startLogout on button click

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -7,3 +7,9 @@ test("should render Header correctly", () => {
     expect(wrapper).toMatchSnapshot();
 });
 
+test("should call startLogout on button click", () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find("button").simulate("click");
+    expect(startLogout).toHaveBeenCalled();
+});
