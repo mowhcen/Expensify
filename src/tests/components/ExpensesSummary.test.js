7ede84d701a263220a2d2f2e81a83a47973450c8
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:45:33 +0330

    test: render ExpensesSummary with multiple expenses

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -8,3 +8,10 @@ test("should correctly render ExpensesSummary with 1 expense", () => {
     );
     expect(wrapper).toMatchSnapshot();
 });
+
+test("should correctly render ExpensesSummary with multiple expenses", () => {
+    const wrapper = shallow(
+        <ExpensesSummary expenseCount={23} expensesTotal={254654634} />
+    );
+    expect(wrapper).toMatchSnapshot();
+});
