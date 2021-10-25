88058c37195483c105563c78325c4113b423c97f
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:28:07 +0330

    test: should generate logout action object

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -9,3 +9,9 @@ test("should generate login action object", () => {
     });
 });
 
+test("should generate logout action object", () => {
+    const action = logout();
+    expect(action).toEqual({
+        type: "LOGOUT",
+    });
+});
