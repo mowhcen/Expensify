82f20fd95c3b45e59c50937dc486f60939d58a77
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:40:27 +0330

    test: render ExpenseList with expenses

---
+++ src/tests/components/ExpenseList.test.js
@@ -0,0 +1,9 @@
+import { ExpenseList } from "../../components/ExpenseList";
+import React from "react";
+import expenses from "../fixtures/expenses";
+import { shallow } from "enzyme";
+
+test("should render ExpenseList with expenses", () => {
+    const wrapper = shallow(<ExpenseList expenses={expenses} />);
+    expect(wrapper).toMatchSnapshot();
+});
