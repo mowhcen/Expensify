ce2d3d2b523e05f8e4f6b17c56d488c66c036277
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:37:24 +0330

    test: render ExpenseForm correctly

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,10 @@
+import ExpenseForm from "../../components/ExpenseForm";
+import React from "react";
+import expenses from "../fixtures/expenses";
+import moment from "moment";
+import { shallow } from "enzyme";
+
+test("should render ExpenseForm correctly", () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+});
