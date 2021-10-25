04514b7835ce9818e785f8f5156c9637cc478209
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:42:39 +0330

    test: set up dummy data for list filter component

---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,26 @@
+import { altFilters, filters } from "../fixtures/filters";
+
+import { ExpenseListFilters } from "../../components/ExpenseListFilters";
+import React from "react";
+import moment from "moment";
+import { shallow } from "enzyme";
+
+let wrapper, setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate;
+
+beforeEach(() => {
+    setTextFilter = jest.fn();
+    sortByDate = jest.fn();
+    sortByAmount = jest.fn();
+    setStartDate = jest.fn();
+    setEndDate = jest.fn();
+    wrapper = shallow(
+        <ExpenseListFilters
+            filters={filters}
+            setTextFilter={setTextFilter}
+            sortByDate={sortByDate}
+            sortByAmount={sortByAmount}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+        />
+    );
+});
