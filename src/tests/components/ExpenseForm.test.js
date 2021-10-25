367d4f1138860007b077cf365121c9707b8e31b5
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:38:53 +0330

    test: not set amount if invalid input

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -50,3 +50,12 @@ test("should set amount if valid input", () => {
     });
     expect(wrapper.state("amount")).toBe(value);
 });
+
+test("should not set amount if invalid input", () => {
+    const value = "23.255";
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find("input").at(1).simulate("change", {
+        target: { value },
+    });
+    expect(wrapper.state("amount")).toBe("");
+});
