5a956f38896a8d20e39c9365720a4820609c6962
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:29:04 +0330

    test: create dummy data for mock store

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,37 @@
+import configureMockStore from "redux-mock-store";
+import database from "../../firebase/firebase";
+import expenses from "../fixtures/expenses";
+import thunk from "redux-thunk";
+import uuid from "uuid";
+
+const id = uuid();
+
+const uid = "garyhahbdafhhd";
+
+const defaultAuthState = {
+    auth: {
+        uid,
+    },
+};
+const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+    const expensesData = {};
+    expenses.forEach(({ id, description, note, amount, createAt }) => {
+        expensesData[id] = { description, note, amount, createAt };
+    });
+
+    database
+        .ref(`users/${uid}/expenses`)
+        .set(expensesData)
+        .then(() => {
+            done();
+        });
+});
+
+const sample_obj = {
+    description: "new item",
+    note: "sample note",
+    amount: 1234,
+    createAt: 123456789,
+};
