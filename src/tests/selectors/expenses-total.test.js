9c416e0453b23e1b024cbafdc26931a486ac128d
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 14:00:18 +0330

    test: correctly add up a single expense

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -5,3 +5,9 @@ test("should return 0 if no expense", () => {
     const total = getExpenseTotal([]);
     expect(total).toEqual(0);
 });
+
+test("should correctly add up a single expense", () => {
+    const total = getExpenseTotal([expenses[0]]);
+    expect(total).toEqual(expenses[0].amount);
+});
+
