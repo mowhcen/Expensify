8feda3d47efdfbdcdc328613be15334610a82b11
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:43:51 +0330

    test: sort by date

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -43,3 +43,14 @@ test("should handle text change", () => {
     });
     expect(setTextFilter).toHaveBeenLastCalledWith(value);
 });
+
+test("should sort by date", () => {
+    const value = "date";
+    wrapper.setProps({
+        filters: altFilters,
+    });
+    wrapper.find("select").simulate("change", {
+        target: { value },
+    });
+    expect(sortByDate).toBeCalledWith();
+});
