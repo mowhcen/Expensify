10fff91eb48e5b5d2286236eb4d1b91bd824863f
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:48:20 +0330

    test: set uid for login

---
+++ src/tests/reducers/auth.test.js
@@ -0,0 +1,11 @@
+import authReducer from "../../reducers/auth";
+
+test("should set uid for login", () => {
+    const action = {
+        type: "LOGIN",
+        uid: "abc123",
+    };
+
+    const state = authReducer({}, action);
+    expect(state.uid).toBe(action.uid);
+});
