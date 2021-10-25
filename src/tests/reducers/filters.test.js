310fbda53363b9a6c05df929ab17b53939381085
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:56:29 +0330

    test: set sortBy to amount

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -11,3 +11,11 @@ test("should setup default filter values", () => {
         endDate: moment().endOf("month"),
     });
 });
+
+test("should set sortBy to amount", () => {
+    const state = filterReducer(undefined, { type: "SORT_BY_AMOUNT" });
+
+    expect(state.sortBy).toBe("amount");
+});
+
+
