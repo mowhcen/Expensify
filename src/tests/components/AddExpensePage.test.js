8f4f3845462473e7e69a035ec4dc78c9155ec2fc
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-10-25 13:35:41 +0330

    test: render addExpensePage components

---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,24 @@
+import { AddExpensePage } from "../../components/AddExpensePage";
+import React from "react";
+import expenses from "../fixtures/expenses";
+import { shallow } from "enzyme";
+
+let startAddExpense, history, wrapper;
+
+beforeEach(() => {
+    startAddExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(
+        <AddExpensePage startAddExpense={startAddExpense} history={history} />
+    );
+});
+
+test("should render AddExpensePage correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+test("should handle on submit", () => {
+    wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith("/");
+    expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
+});
